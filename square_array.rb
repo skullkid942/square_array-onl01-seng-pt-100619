527b0df2b5e137e1fe802cf1d4a26692e67ee1d2
Bethany Holley <chocolatechipfaced@gmail.com>
2019-10-14 17:11:15 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,7 @@
 def square_array(array)
   numbers = [1,2,3]
-  numbers.each{ |number| puts new_number = number**number}
+  numbers.each do |number| 
+    new_number = number**number 
+    puts new_number
+  end 
 end
